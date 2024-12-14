6fa21cf84a80fc368b085da3f15f2fe3e32f3b5f
yohei.hokari <yohei.hokari@gmail.com>
2024-12-14 22:51:44 +0900

    add: ユーザー一覧をADMIN権限を持つユーザーのみが表示できるようにした

--- src/main/java/com/example/its/domain/auth/UserService.java
+++ src/main/java/com/example/its/domain/auth/UserService.java
@@ -1,6 +1,7 @@
 package com.example.its.domain.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,13 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void create(String username, String password) {
         var encodedPassword = passwordEncoder.encode(password);
         userRepository.insert(username, encodedPassword);
